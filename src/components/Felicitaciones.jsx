37790554143e469b2e7e491e7849a3d26bf03973
AnahiRosado18 <131729763+AnahiRosado18@users.noreply.github.com>
2025-09-21 16:36:41 -0600

    Update Felicitaciones.jsx

--- src/components/Felicitaciones.jsx
+++ src/components/Felicitaciones.jsx
@@ -3,11 +3,13 @@ import React from "react";
 export default function Felicitaciones({ respuestascorrectas, preguntastotales, manejarReproducirDeNuevo }) {
   const pct = Math.round((respuestascorrectas / preguntastotales) * 100);
   return (
-    <div className="center">
-      <div><img src="./assets/congrats.svg" alt="" />🎉</div>
-      <h2>Congrats! You completed the quiz.</h2>
-      <div className="score">You answer {respuestascorrectas}/{preguntastotales}  correctly.</div>
-      <button className="btn primary" onClick={manejarReproducirDeNuevo}>Play again</button>
+    <div className="result-card">
+      <div>
+        <img src="./src/assets/congrats.png" alt="🎉" />
+      </div>
+      <h2 style={{ marginBottom: "5vh", fontWeight: "400"}}>Congrats! You completed the quiz.</h2>
+      <div className="score" style={{ marginBottom: "5vh"}}>You answer {respuestascorrectas}/{preguntastotales}  correctly.</div>
+      <button className="btn-fin" style={{ fontWeight: "400"}} onClick={manejarReproducirDeNuevo}>Play again</button>
     </div>
   );
 }
