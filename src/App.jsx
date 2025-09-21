95f3886f8f48270ca8721f5f92b1994d4df3d768
AnahiRosado18 <131729763+AnahiRosado18@users.noreply.github.com>
2025-09-21 16:39:05 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,8 @@ export default function App() {
   return (
     <main className="container">
       <header className="row space-between">
-        <h1>Country Quiz</h1>
       </header>
       <Quiz />
-      <footer className="footer muted small">Datos por restcountries.com</footer>
     </main>
   );
 }
